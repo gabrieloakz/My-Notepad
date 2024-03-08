4f3508a6e598ed31962e95fa6f2071af2120ef80
Gabriel Ferreira Carvalho <118980722+gabrieloakz@users.noreply.github.com>
2024-03-08 09:59:29 +0000

    initial commit
    menu nav bar and icons

---
+++ My Notepad/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace My_Notepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.menuFicheiro = new System.Windows.Forms.ToolStripSplitButton();
+            this.ficheiroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEditar = new System.Windows.Forms.ToolStripSplitButton();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFormatar = new System.Windows.Forms.ToolStripSplitButton();
+            this.formatarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFicheiroNovo = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(30, 30);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuFicheiro,
+            this.menuEditar,
+            this.menuFormatar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(784, 37);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // menuFicheiro
+            // 
+            this.menuFicheiro.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ficheiroToolStripMenuItem,
+            this.menuFicheiroNovo,
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.menuFicheiro.Image = ((System.Drawing.Image)(resources.GetObject("menuFicheiro.Image")));
+            this.menuFicheiro.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuFicheiro.Name = "menuFicheiro";
+            this.menuFicheiro.Size = new System.Drawing.Size(95, 34);
+            this.menuFicheiro.Text = "Ficheiro";
+            // 
+            // ficheiroToolStripMenuItem
+            // 
+            this.ficheiroToolStripMenuItem.Name = "ficheiroToolStripMenuItem";
+            this.ficheiroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ficheiroToolStripMenuItem.Text = "Ficheiro";
+            // 
+            // menuEditar
+            // 
+            this.menuEditar.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editarToolStripMenuItem});
+            this.menuEditar.Image = ((System.Drawing.Image)(resources.GetObject("menuEditar.Image")));
+            this.menuEditar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuEditar.Name = "menuEditar";
+            this.menuEditar.Size = new System.Drawing.Size(83, 34);
+            this.menuEditar.Text = "Editar";
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // menuFormatar
+            // 
+            this.menuFormatar.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formatarToolStripMenuItem});
+            this.menuFormatar.Image = ((System.Drawing.Image)(resources.GetObject("menuFormatar.Image")));
+            this.menuFormatar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuFormatar.Name = "menuFormatar";
+            this.menuFormatar.Size = new System.Drawing.Size(101, 34);
+            this.menuFormatar.Text = "Formatar";
+            // 
+            // formatarToolStripMenuItem
+            // 
+            this.formatarToolStripMenuItem.Name = "formatarToolStripMenuItem";
+            this.formatarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.formatarToolStripMenuItem.Text = "Formatar";
+            // 
+            // menuFicheiroNovo
+            // 
+            this.menuFicheiroNovo.Name = "menuFicheiroNovo";
+            this.menuFicheiroNovo.Size = new System.Drawing.Size(180, 22);
+            this.menuFicheiroNovo.Text = "Novo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 741);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "My Notepad.";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSplitButton menuFicheiro;
+        private System.Windows.Forms.ToolStripMenuItem ficheiroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSplitButton menuEditar;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSplitButton menuFormatar;
+        private System.Windows.Forms.ToolStripMenuItem formatarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuFicheiroNovo;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
+
